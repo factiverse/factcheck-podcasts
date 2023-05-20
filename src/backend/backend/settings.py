6c822d971df5a98a4676809891e4978424aecc2f
adjabe <adamjbecker001@gmail.com>
2023-05-21 01:23:16 +0200

    sync with server

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-csiaa5)onm*33--t*bk+8z(=ylin5&gg4#i1b8!p$bu!mkth94'
+SECRET_KEY = 'django-insecure-csiaa5)onm*sdfasjdhekj5&gg4#i1b8!p$bu!mkth94'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
---
+++ src/backend/setVisibility.sql
@@ -0,0 +1,3 @@
+UPDATE api_utterance
+SET visibility =  '["Transcription", "Advertising", "Diarization"]'
+WHERE segmentation_id = 2281
