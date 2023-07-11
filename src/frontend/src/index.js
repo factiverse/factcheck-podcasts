f9845f26bf6914d7869e7bc2717b459f17960733
Gemblacensis <260698@uis.no>
2023-07-11 10:54:26 +0200

    swapped ports

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -14,7 +14,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import PodcastBrowser from './podcasts/PodcastBrowser';
 
 //dev
-axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = 'http://localhost:8888/';
 //axios.defaults.baseURL = 'http://192.168.2.239/';
 //axios.defaults.baseURL = 'http://192.168.1.145:8888/';
 //docker publish
